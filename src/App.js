0cd69ecb22021332017beeff9f0591665b6a0016
magnuvau <austrheim.magnus@protonmail.com>
2020-04-13 13:33:05 +0200

    JSX stuff

--- src/App.js
+++ src/App.js
@@ -4,16 +4,23 @@ import './App.css';
 
 class App extends Component {
   render() {
+
+    var i = 1;
+
+    var myStyle = {
+       fontSize: 100,
+       color: '#FF0000'
+    }
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+     <div>
+        <h1>Header</h1>
+        <h2 style = {myStyle}>Stylish header</h2>
+        {/*Comment: This is JSX, not HTML!*/}
+        <p>A paragraph</p>
+        <p>Javascript expression: {i} + {i} = {i + i}</p>
+        <p>Conditional expression: {i} == {i} {i == 1 ? 'True' : 'False'}</p>
+     </div>
     );
   }
 }
