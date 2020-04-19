2e11ecae67b11d52ac8db1d6a09bf72605cd75ec
magnuvau <austrheim.magnus@protonmail.com>
2020-04-19 13:31:31 +0200

    Add stateless and stateful components

--- src/App.js
+++ src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+   constructor() {
+      super();
+      this.state = {
+         data:
+         [
+            {
+               "id":1,
+               "name":"Foo",
+               "age":"20"
+            }
+         ]
+      }
+   }
   render() {
 
     var i = 1;
@@ -15,14 +28,58 @@ class App extends Component {
     return (
      <div>
         <h1>Header</h1>
-        <h2 style = {myStyle}>Stylish header</h2>
+        <h2 style={myStyle}>Stylish header</h2>
         {/*Comment: This is JSX, not HTML!*/}
         <p>A paragraph</p>
         <p>Javascript expression: {i} + {i} = {i + i}</p>
-        <p>Conditional expression: {i} == {i} {i == 1 ? 'True' : 'False'}</p>
+        <p>Conditional expression: {i} === {i} {i === 1 ? 'True' : 'False'}</p>
+
+        {/*Insert other components*/}
+        <StatelessComponent1/>
+        <StatelessComponent2/>
+
+        {/*Create table of dats from this stateful component App*/}
+        <table>
+           <tbody>
+              {this.state.data.map((person, i)=><TableRow key={i}
+                 data={person} />)}
+           </tbody>
+        </table>
      </div>
     );
   }
+}
+
+class StatelessComponent1 extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>StatelessComponent1</h1>
+         </div>
+      );
+   }
+}
+
+class StatelessComponent2 extends React.Component {
+   render() {
+      return (
+         <div>
+            <h1>StatelessComponent2</h1>
+         </div>
+      );
+   }
+}
+
+class TableRow extends React.Component {
+   render() {
+      return (
+         <tr>
+            <td>{this.props.data.id}</td>
+            <td>{this.props.data.name}</td>
+            <td>{this.props.data.age}</td>
+         </tr>
+      );
+   }
 }
 
 export default App;
